f420744fb055b6787acdb2738f70e643d720c5f1
Alex Mena <amena@lukapay.io>
2025-03-17 01:11:19 +0100

    Optimizar fuentes de Google en document.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,21 +3,12 @@ import { SessionProvider } from "next-auth/react";
 import "../styles/loading-icon.css";
 import "../styles/pagination.css";
 import { AppProps } from "next/app";
-import { Open_Sans } from "next/font/google";
-
-const openSans = Open_Sans({
-  subsets: ["latin"],
-  weight: ["300", "400", "600", "800"],
-  display: "swap",
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider>
-        <main className={openSans.className}>
-          <Component {...pageProps} />
-        </main>
+        <Component {...pageProps} />
       </ThemeProvider>
     </SessionProvider>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,29 @@ import React, { JSX } from "react";
 export default function Document(): JSX.Element {
   return (
     <Html lang="en">
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;800&display=swap"
+          rel="stylesheet"
+          media="print"
+        />
+        <noscript>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;800&display=swap"
+            rel="stylesheet"
+          />
+        </noscript>
+      </Head>
       <body>
         <Main />
         <NextScript />
