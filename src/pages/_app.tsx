c3a7a7bd321011646bfc9d61b723214b07504db0
Alex Mena <amena@lukapay.io>
2025-03-17 01:04:09 +0100

    Switch Fonts to Next/Fonts from Google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,21 @@ import { SessionProvider } from "next-auth/react";
 import "../styles/loading-icon.css";
 import "../styles/pagination.css";
 import { AppProps } from "next/app";
+import { Open_Sans } from "next/font/google";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "600", "800"],
+  display: "swap",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider>
-        <Component {...pageProps} />
+        <main className={openSans.className}>
+          <Component {...pageProps} />
+        </main>
       </ThemeProvider>
     </SessionProvider>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,14 +4,6 @@ import React, { JSX } from "react";
 export default function Document(): JSX.Element {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
       <body>
         <Main />
         <NextScript />
